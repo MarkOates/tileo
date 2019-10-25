f993e9e702a483397dd54f098b4059b7adf84b07
Mark Oates <markoates0@gmail.com>
2019-10-25 18:37:46 -0400

    Remove "set_tile_to_mesh" and "set_tile_uv" from TileMap

--- include/adventures_of_beary/TileMap.hpp
+++ include/adventures_of_beary/TileMap.hpp
@@ -12,8 +12,6 @@
 class TileMap
 {
 private:
-   void set_tile_uv(int tile_x, int tile_y, int u1, int v1, int u2, int v2);
-
    ALLEGRO_VERTEX_BUFFER *vertex_buffer;
    std::vector<ALLEGRO_VERTEX> vtx;
    int width;
@@ -33,7 +31,6 @@ public:
 
    int get_tile(int tile_x, int tile_y);
    bool set_tile(TileAtlas &tile_atlas, TileMapMeshRenderer &mesh_renderer, int tile_x, int tile_y, int tile_index);
-   bool set_tile_to_mesh(TileAtlas &tile_atlas, int tile_x, int tile_y, int tile_index);
    //bool set_contiguous_tile_num(TileAtlas &tile_atlas, int contiguous_tile_num, int tile_index);
 
    void resize(TileMapMeshRenderer &mesh_renderer, int w, int h, int tile_w, int tile_h);
--- src/adventures_of_beary/TileMap.cpp
+++ src/adventures_of_beary/TileMap.cpp
@@ -5,49 +5,6 @@
 #include <iostream>
 
 
-void TileMap::set_tile_uv(int tile_x, int tile_y, int u1, int v1, int u2, int v2)
-{
-   int index_start = (tile_x * 6) + tile_y * (width*6);
-   int &i = index_start;
-
-   //ALLEGRO_VERTEX *vbuff = (ALLEGRO_VERTEX *)al_lock_vertex_buffer(vertex_buffer, index_start, 6, ALLEGRO_LOCK_WRITEONLY);
-   ALLEGRO_VERTEX *vbuff = (ALLEGRO_VERTEX *)al_lock_vertex_buffer(vertex_buffer, index_start, 6, ALLEGRO_LOCK_WRITEONLY);
-   if (!vbuff) std::cout << "could not lock vertex buffer" << std::endl;
-
-   if (use_vtx) vtx[i+0].u = u1;
-   if (use_vtx) vtx[i+0].v = v1;
-   vbuff[0].u = u1;
-   vbuff[0].v = v1;
-
-   if (use_vtx) vtx[i+1].u = u1;
-   if (use_vtx) vtx[i+1].v = v2;
-   vbuff[1].u = u1;
-   vbuff[1].v = v2;
-
-   if (use_vtx) vtx[i+2].u = u2;
-   if (use_vtx) vtx[i+2].v = v2;
-   vbuff[2].u = u2;
-   vbuff[2].v = v2;
-
-   if (use_vtx) vtx[i+3].u = u2;
-   if (use_vtx) vtx[i+3].v = v2;
-   vbuff[3].u = u2;
-   vbuff[3].v = v2;
-
-   if (use_vtx) vtx[i+4].u = u2;
-   if (use_vtx) vtx[i+4].v = v1;
-   vbuff[4].u = u2;
-   vbuff[4].v = v1;
-
-   if (use_vtx) vtx[i+5].u = u1;
-   if (use_vtx) vtx[i+5].v = v1;
-   vbuff[5].u = u1;
-   vbuff[5].v = v1;
-
-   al_unlock_vertex_buffer(vertex_buffer);
-}
-
-
 TileMap::TileMap()
    : vertex_buffer(NULL)
    , vtx()
@@ -100,27 +57,6 @@ bool TileMap::set_tile(TileAtlas &tile_atlas, TileMapMeshRenderer &mesh_renderer
    tiles[tile_x + tile_y * width] = tile_index;
 
    mesh_renderer.set_tile_to_mesh(tile_atlas, tile_x, tile_y, tile_index);
-
-   return true;
-}
-
-
-bool TileMap::set_tile_to_mesh(TileAtlas &tile_atlas, int tile_x, int tile_y, int tile_index)
-{
-   // if the tile index does not exist in the tile_atlas, break out
-   if (tile_index >= (int)tile_atlas.get_tile_index().size()) return false;
-
-   // if the tile_index is a negative number, use the number "0" instead
-   // I'm not sure how/why this is the preferred approach.  I think negative numbers
-   // should be allowed, any number should be allowed.  So this should be revisited
-   if (tile_index < 0) tile_index = 0;
-
-   // transfer the uv coordinates of the from the tile atlas bitmap to the mesh
-   // {
-      int u1, v1, u2, v2;
-      tile_atlas.get_tile_uv(tile_index, &u1, &v1, &u2, &v2);
-      set_tile_uv(tile_x, tile_y, u1, v1, u2, v2);
-   // }
 
    return true;
 }
