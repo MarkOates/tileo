c9e4fcecf690951f590c3d66dc07a7de88238733
Mark Oates <markoates0@gmail.com>
2019-10-25 17:18:34 -0400

    Add "TileAtlasTest"

---
+++ tests/adventures_of_beary/TileAtlasTest.cpp
@@ -0,0 +1,10 @@
+
+#include <gtest/gtest.h>
+
+#include <adventures_of_beary/TileAtlas.hpp>
+
+TEST(adventures_of_beary__TileAtlasTest, can_be_created_without_blowing_up)
+{
+   TileAtlas tile_atlas;
+}
+
