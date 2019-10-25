5d00be357101a054e04b3e825b42211894d5ef3e
Mark Oates <markoates0@gmail.com>
2019-10-25 17:38:44 -0400

    Duplicate draw() functionality in TileMapMeshRenderer::render()

--- include/adventures_of_beary/TileMapMeshRenderer.hpp
+++ include/adventures_of_beary/TileMapMeshRenderer.hpp
@@ -1,15 +1,21 @@
 #pragma once
 
 
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_primitives.h>
 #include <string>
 
 
 class TileMapMeshRenderer
 {
 private:
+  ALLEGRO_VERTEX_BUFFER *vertex_buffer;
+  ALLEGRO_BITMAP *tile_atlas_bitmap;
+  int camera_x;
+  int camera_y;
 
 public:
-   TileMapMeshRenderer();
+   TileMapMeshRenderer(ALLEGRO_BITMAP *tile_atlas_bitmap=nullptr, int camera_x=0, int camera_y=0);
    ~TileMapMeshRenderer();
 
    void render();
--- src/adventures_of_beary/TileMapMeshRenderer.cpp
+++ src/adventures_of_beary/TileMapMeshRenderer.cpp
@@ -3,7 +3,11 @@
 #include <adventures_of_beary/TileMapMeshRenderer.hpp>
 
 
-TileMapMeshRenderer::TileMapMeshRenderer()
+TileMapMeshRenderer::TileMapMeshRenderer(ALLEGRO_BITMAP *tile_atlas_bitmap, int camera_x, int camera_y)
+   : vertex_buffer(nullptr)
+   , tile_atlas_bitmap(tile_atlas_bitmap)
+   , camera_x(camera_x)
+   , camera_y(camera_y)
 {
 }
 
@@ -15,6 +19,16 @@ TileMapMeshRenderer::~TileMapMeshRenderer()
 
 void TileMapMeshRenderer::render()
 {
+   ALLEGRO_TRANSFORM prev, transform;
+   al_copy_transform(&prev, al_get_current_transform());
+   al_identity_transform(&transform);
+   al_translate_transform(&transform, -camera_x, -camera_y);
+   al_use_transform(&transform);
+
+   //al_draw_prim(&vtx[0], NULL, tile_atlas->bitmap, 0, vtx.size(), ALLEGRO_PRIM_TRIANGLE_LIST);
+   al_draw_vertex_buffer(vertex_buffer, tile_atlas_bitmap, 0, al_get_vertex_buffer_size(vertex_buffer), ALLEGRO_PRIM_TRIANGLE_LIST);
+
+   al_use_transform(&prev);
 }
 
 
