98a5782113bdb417765452ef308811a5e10ac000
Mark Oates <markoates0@gmail.com>
2019-10-25 17:17:57 -0400

    Add TileIndexInfoTest

---
+++ tests/adventures_of_beary/TileIndexInfoTest.cpp
@@ -0,0 +1,10 @@
+
+#include <gtest/gtest.h>
+
+#include <adventures_of_beary/TileIndexInfo.hpp>
+
+TEST(adventures_of_beary__TileIndexInfoTest, can_be_created_without_blowing_up)
+{
+   TileIndexInfo tile_index_info;
+}
+
