0bba3ebae323371b2bf43dd2513c2018e49df2d7
Mark Oates <markoates0@gmail.com>
2019-10-26 10:12:31 -0400

    Add some extra cosmetic blank lines to test file

--- tests/adventures_of_beary/TileMapTest.cpp
+++ tests/adventures_of_beary/TileMapTest.cpp
@@ -3,10 +3,12 @@
 
 #include <adventures_of_beary/TileMap.hpp>
 
+
 TEST(adventures_of_beary__TileMapTest, can_be_created_without_blowing_up)
 {
    TileMap tile_map;
 }
+
 
 TEST(adventures_of_beary__TileMapTest, initializes_with_the_expected_values)
 {
@@ -15,9 +17,11 @@ TEST(adventures_of_beary__TileMapTest, initializes_with_the_expected_values)
    ASSERT_EQ(0, tile_map.get_height());
 }
 
+
 TEST(adventures_of_beary__TileMapTest, is_dimensionless__returns_true_if_the_width_or_height_is_less_than_zero)
 {
    TileMap tile_map;
    ASSERT_EQ(true, tile_map.is_dimensionless());
 }
+
 
