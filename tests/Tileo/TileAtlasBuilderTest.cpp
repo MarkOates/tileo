9a5748008a57906821cc7e314c6ddb310668a7ee
Mark Oates <markoates0@gmail.com>
2022-03-09 21:44:06 -0500

    * Fix test description and remove #dev# test

--- tests/Tileo/TileAtlasBuilderTest.cpp
+++ tests/Tileo/TileAtlasBuilderTest.cpp
@@ -24,7 +24,7 @@ TEST(Tileo_TileAtlasBuilderTest, can_be_created_without_blowing_up)
 }
 
 
-TEST(Tileo_TileAtlasBuilderTest, build__will_create_an_atlas_with_tile_edges_extruded)
+TEST(Tileo_TileAtlasBuilderTest, build_extruded__will_create_an_atlas_with_tile_edges_extruded)
 {
    al_init();
    al_init_image_addon();
@@ -48,38 +48,12 @@ TEST(Tileo_TileAtlasBuilderTest, build__will_create_an_atlas_with_tile_edges_ext
 }
 
 
-TEST(Tileo_TileAtlasBuilderTest, dev)
+
+TEST(Tileo_TileAtlasBuilderTest,
+   build_scaled_and_extruded__will_create_a_pixel_perfect_scaled_atlas_with_tile_edges_extruded)
 {
-   al_init();
-   al_init_image_addon();
-   //al_init_color_addon();
-
-   ALLEGRO_BITMAP* source_bitmap = al_load_bitmap(TEST_TILE_ATLAS_BITMAP_PATH);
-   ASSERT_NE(nullptr, source_bitmap);
-
-   //Tileo::Atlas atlas;
-   //atlas.duplicate_bitmap_and_load(source_bitmap, 16, 16, 0);
-
-   int scale=3;
-   ALLEGRO_BITMAP *scaled = TileAtlasBuilder::create_pixel_perfect_scaled_render(source_bitmap, scale);
-
-   Tileo::Atlas atlas;
-   atlas.duplicate_bitmap_and_load(scaled, 16*scale, 16*scale, 0);
-
-   al_destroy_bitmap(scaled);
-
-   std::vector<Tileo::TileIndexInfo> tile_index = atlas.get_tile_index();
-   TileAtlasBuilder tile_atlas_builder(16*scale, 16*scale, tile_index);
-   ALLEGRO_BITMAP *result = tile_atlas_builder.build_extruded();
-   al_save_bitmap(build_test_filename_png("buid__will_create_an_atlas").c_str(), result);
-
-
-   //al_destroy_bitmap(scaled);
-   al_destroy_bitmap(result);
-   //atlas.clear();
-   al_destroy_bitmap(source_bitmap);
-   al_shutdown_image_addon();
-   al_uninstall_system();
+   // TODO
 }
+
 
 
