f5e5e5231570e27adf2d761a4ad2ce28782984f5
Mark Oates <markoates0@gmail.com>
2022-03-30 13:01:10 -0400

    * Add test to validate #num_columns# and #num_rows# are greater than zero

--- tests/Tileo/MeshWithNormalsTest.cpp
+++ tests/Tileo/MeshWithNormalsTest.cpp
@@ -1,28 +1,65 @@
 
 #include <gtest/gtest.h>
 
+#define EXPECT_THROW_WITH_MESSAGE(code, raised_exception_type, expected_exception_message) \
+   try { code; FAIL() << "Expected " # raised_exception_type; } \
+   catch ( raised_exception_type const &err ) { EXPECT_EQ(std::string(expected_exception_message), err.what()); } \
+   catch (...) { FAIL() << "Expected " # raised_exception_type; }
+
 #include <Tileo/MeshWithNormals.hpp>
 
 
-TEST(Tileo_MeshWithNormalsTest, can_be_created_without_blowing_up)
+class Tileo_MeshWithNormalsRenderingFixtureTest : public ::testing::Test
+{
+private:
+   ALLEGRO_DISPLAY *display;
+
+public:
+   virtual void SetUp() override
+   {
+      al_init();
+      al_init_primitives_addon();
+      al_set_new_display_flags(ALLEGRO_PROGRAMMABLE_PIPELINE);
+      ALLEGRO_DISPLAY *display = al_create_display(800, 600);
+   }
+
+   virtual void TearDown() override
+   {
+      al_destroy_display(display);
+      al_shutdown_primitives_addon();
+      al_uninstall_system();
+   }
+};
+
+class Tileo_MeshWithNormalsTestFixture : public ::testing::Test {};
+
+
+TEST_F(Tileo_MeshWithNormalsTestFixture, can_be_created_without_blowing_up)
 {
    Tileo::MeshWithNormals mesh_with_normals;
 }
 
 
-TEST(Tileo_MeshWithNormalsTest, initialize__works_without_blowing_up)
+TEST_F(Tileo_MeshWithNormalsRenderingFixtureTest, initialize__works_without_blowing_up)
 {
-   al_init();
-   al_init_primitives_addon();
-   al_set_new_display_flags(ALLEGRO_PROGRAMMABLE_PIPELINE);
-   ALLEGRO_DISPLAY *display = al_create_display(800, 600);
-   
    Tileo::MeshWithNormals mesh_with_normals(6, 4, 16, 16);
    mesh_with_normals.initialize();
+}
 
-   al_destroy_display(display);
-   al_shutdown_primitives_addon();
-   al_uninstall_system();
+
+TEST_F(Tileo_MeshWithNormalsRenderingFixtureTest, initialize__with_num_columns_less_than_zero__throws_an_error)
+{
+   Tileo::MeshWithNormals mesh_with_normals(-1, 4, 16, 16);
+   std::string expected_error_message = "MeshWithNormals::initialize: error: guard \"(num_columns >= 0)\" not met";
+   EXPECT_THROW_WITH_MESSAGE(mesh_with_normals.initialize(), std::runtime_error, expected_error_message);
+}
+
+
+TEST_F(Tileo_MeshWithNormalsRenderingFixtureTest, initialize__with_num_rows_less_than_zero__throws_an_error)
+{
+   Tileo::MeshWithNormals mesh_with_normals(6, -1, 16, 16);
+   std::string expected_error_message = "MeshWithNormals::initialize: error: guard \"(num_rows >= 0)\" not met";
+   EXPECT_THROW_WITH_MESSAGE(mesh_with_normals.initialize(), std::runtime_error, expected_error_message);
 }
 
 
