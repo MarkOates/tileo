4242ab34bd97fc2648ceb27b2fa3d8d9db383300
Mark Oates <markoates0@gmail.com>
2022-03-30 14:28:30 -0400

    * Add test for #build_empty_vertex#

--- tests/Tileo/TileoTileVertexAllegroVertexDeclarationTest.cpp
+++ tests/Tileo/TileoTileVertexAllegroVertexDeclarationTest.cpp
@@ -13,6 +13,36 @@
    (#class_name "::" #method_name ": error: guard \"" #guard_statement "\" not met")
 
 
+static bool operator==(const ALLEGRO_COLOR& color1, const ALLEGRO_COLOR& color2)
+{
+   return (color1.r == color2.r)
+      && (color1.g == color2.g)
+      && (color1.b == color2.b)
+      && (color1.a == color2.a);
+}
+
+static bool operator!=(const ALLEGRO_COLOR& color1, const ALLEGRO_COLOR& color2)
+{
+   return (color1.r != color2.r)
+      || (color1.g != color2.g)
+      || (color1.b != color2.b)
+      || (color1.a != color2.a);
+}
+
+bool operator==(const TILEO_TILE_VERTEX &v1, const TILEO_TILE_VERTEX &v2)
+{
+  if (v1.x != v2.x) return false;
+  if (v1.y != v2.y) return false;
+  if (v1.z != v2.z) return false;
+  if (v1.texture_u != v2.texture_v) return false;
+  if (v1.normal_u != v2.normal_v) return false;
+  if (v1.height_u != v2.height_v) return false;
+  if (v1.height != v2.height) return false;
+  if (v1.color != v2.color) return false;
+  return true;
+}
+
+
 TEST(Tileo_TileoTileVertexAllegroVertexDeclarationTest, can_be_created_without_blowing_up)
 {
    Tileo::TileoTileVertexAllegroVertexDeclaration declaration;
@@ -197,6 +227,16 @@ TEST(Tileo_TileoTileVertexAllegroVertexDeclarationTest, get_vertex_declaration__
    declaration.destroy();
    al_shutdown_primitives_addon();
    al_uninstall_system();
+}
+
+
+TEST(Tileo_TileoTileVertexAllegroVertexDeclarationTest,
+   build_empty_vertex__returns_a_vertex_with_zeroed_values_and_white_color_value)
+{
+   Tileo::TileoTileVertexAllegroVertexDeclaration declaration;
+   TILEO_TILE_VERTEX expected_vertex{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, {1, 1, 1, 1}};
+   TILEO_TILE_VERTEX actual_vertex = declaration.build_empty_vertex();
+   EXPECT_EQ(expected_vertex, actual_vertex);
 }
 
 
