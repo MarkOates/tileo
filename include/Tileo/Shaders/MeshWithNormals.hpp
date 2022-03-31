c3d4eb085e2568b8dd51c13ac35036a773143e71
Mark Oates <markoates0@gmail.com>
2022-03-31 12:04:16 -0400

    * Duplicate Shaders/MeshWithNormals (from Hexagon/../FlatColor)

---
+++ include/Tileo/Shaders/MeshWithNormals.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+
+#include <Tileo/Shaders/Base.hpp>
+#include <allegro5/allegro.h>
+#include <string>
+
+
+namespace Tileo
+{
+   namespace Shaders
+   {
+      class MeshWithNormals : private Tileo::Shaders::Base
+      {
+      private:
+         bool initialized;
+
+      public:
+         MeshWithNormals();
+         ~MeshWithNormals();
+
+         void initialize();
+         void activate();
+         void set_flat_color(ALLEGRO_COLOR flat_color={});
+         static std::string obtain_vertex_source();
+         static std::string obtain_fragment_source();
+      };
+   }
+}
+
+
+
